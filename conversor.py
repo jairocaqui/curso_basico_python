b8c88b14f61dd156ec5f32594c9302f89a57fc5b
Jairo Caqui <jairo.caqui@gmail.com>
2020-10-28 09:47:04 -0500

    Primer commit de python basico

---
+++ conversor.py
@@ -0,0 +1,9 @@
+soles= input("Ingrese la cantidad de soles  a convertir??   ")
+soles=float(peso)
+valor_dollar=3.63  
+dolares=peso/ valor_dollar
+dolares=round(dolares,2)
+
+dolares=str(dolares)
+print("Tienes $"+ dolares+ " dolares")
+
